e7a7c1f9d9e54ca24af5d363dc1f8669df4eaa36
Mateus <mateus-botto@educar.rs.gov.br>
2023-08-04 11:38:56 -0300

    Ajustes de layout

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const tarefaController = require('./controllers/tarefaController');
 const homeController = require('./controllers/homeController');
 const app = express();
 const port = 5000;
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(expressLayouts);
+app.set('layout', './layouts/default/index');
 app.use(express.urlencoded({ extended: true }));
 app.get('/home', homeController.getView);
 app.get('/tarefas', tarefaController.getTarefas);
